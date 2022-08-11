1c88d6db4869c3f32d6ec542c1d731c43c581864
at-grigorievich <at.grigorievich@gmail.com>
2022-08-11 12:03:25 +0300

    Update texture data SO

--- TableDropGame/Assets/Scripts/Spawner Logic/Data/TextureParameters.cs
+++ TableDropGame/Assets/Scripts/Spawner Logic/Data/TextureParameters.cs
@@ -1,54 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
-using Cysharp.Threading.Tasks;
 using UnityEngine;
-using UnityEngine.Networking;
 
 namespace ATG.TableDrop
 {
-    [CreateAssetMenu(fileName = "Texture Data", menuName = "Spawner/New Texture Data", order = 0)]
-    public class UniqueTexture: ScriptableObject
-    {
-        [SerializeField] private string textureName;
-        [Space(10)] 
-        [SerializeField] private bool _isLoadFromWWW;
-        
-        public async UniTask<Texture2D> LoadTexture()
-        {
-            return !_isLoadFromWWW
-                ? await DownloadFromLocal($"{Application.dataPath}/StreamingAssets/Sprites/{textureName}.png")
-                : await DownloadFromWWW(textureName);
-        }
-
-        private static async UniTask<Texture2D> DownloadFromLocal(string path)
-        {
-            var file = new FileInfo(path);
-            byte[] bytes;
-            
-            if (file.Exists)
-            {
-                bytes = await File.ReadAllBytesAsync(path);
-
-                var texture = new Texture2D(1, 1);
-                texture.LoadImage(bytes);
-                
-                return texture;
-            }
-            throw new NullReferenceException("Cant find path " + path);
-        }
-        private static async UniTask<Texture2D> DownloadFromWWW(string path)
-        {
-            using var request = UnityWebRequestTexture.GetTexture(path);
-
-            await request.SendWebRequest();
-            
-            return  request.result == UnityWebRequest.Result.Success
-                ? DownloadHandlerTexture.GetContent(request) : null;
-        }
-    }
-    
     [CreateAssetMenu(fileName = "Texture Values", menuName = "Spawner/New Texture Set", order = 0)]
     public class TextureParameters : ScriptableObject
     {
---
+++ TableDropGame/Assets/Scripts/Spawner Logic/Data/UniqueTexture.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.Networking;
+
+namespace ATG.TableDrop
+{
+    [CreateAssetMenu(fileName = "Texture Data", menuName = "Spawner/New Texture Data", order = 0)]
+    public class UniqueTexture: ScriptableObject
+    {
+        [SerializeField] private string _textureName;
+        [Space(10)] 
+        [SerializeField] private bool _isLoadFromWWW;
+        
+        public async UniTask<Texture2D> LoadTexture()
+        {
+            return !_isLoadFromWWW
+                ? await DownloadFromLocal($"{Application.dataPath}/StreamingAssets/Sprites/{_textureName}.png")
+                : await DownloadFromWWW(_textureName);
+        }
+
+        private static async UniTask<Texture2D> DownloadFromLocal(string path)
+        {
+            var file = new FileInfo(path);
+            byte[] bytes;
+            
+            if (file.Exists)
+            {
+                bytes = await File.ReadAllBytesAsync(path);
+
+                var texture = new Texture2D(1, 1);
+                texture.LoadImage(bytes);
+                
+                return texture;
+            }
+            throw new NullReferenceException("Cant find path " + path);
+        }
+        private static async UniTask<Texture2D> DownloadFromWWW(string path)
+        {
+            using var request = UnityWebRequestTexture.GetTexture(path);
+            
+            await request.SendWebRequest();
+            
+            return  request.result == UnityWebRequest.Result.Success
+                ? DownloadHandlerTexture.GetContent(request) : null;
+        }
+    }
+}
